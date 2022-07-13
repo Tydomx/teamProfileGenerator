0b927187a37759b6b4556df8915f71f00773fb22
Tydomx <michaelthai16@gmail.com>
2022-07-13 13:23:50 -0500

    Added tests for Manager object

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,16 @@
+const Manager = require('../lib/Manager');
+const Employee = require('../lib/Employee');
+
+test("Creating manager's office number", () => {
+	const officeNumber = 111;
+	const employee = new Manager('Michael', 12345, 'test@gmail.com', officeNumber);
+
+	expect(employee.officeNumber).toBe(officeNumber);
+});
+
+test('Use getRole() to return "Manager" as role', () => {
+	const role = 'Manager';
+	const employee = new Manager('Michael', 12345, 'test@gmail.com', 111);
+
+	expect(employee.getRole()).tobe(role);
+});
